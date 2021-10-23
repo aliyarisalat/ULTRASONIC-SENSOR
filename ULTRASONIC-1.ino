397c3d5dab86af168055f7efc8268a45266c9d3d
aliyarisalat <87071721+aliyarisalat@users.noreply.github.com>
2021-10-23 11:59:56 +0500

    Update ULTRASONIC-1.ino

--- ULTRASONIC-1.ino
+++ ULTRASONIC-1.ino
@@ -12,8 +12,8 @@ void setup() {
   pinMode(trigpin,OUTPUT);
   pinMode(echopin,INPUT);
   pinMode(led1,OUTPUT);
-  pinMode(led2,INPUT);
-  pinMode(led3,INPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
   Serial.begin(9600);
 
 }
